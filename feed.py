58c422f25e1bd0096af5d595168e3423f5836b4a
hmr1973 <99620961+hmr1973@users.noreply.github.com>
2022-12-27 13:36:31 +0000

    zfdsfdsf

--- feed.py
+++ feed.py
@@ -28,15 +28,22 @@ value = randint(0, qtde)
 
 titulo = d['entries'][value]['title'] 
 corpo = d['entries'][value]['description'] 
+corpo1 = d['entries'][value]['description']
+
+corpo1 = corpo1.replace("Quero saber mais sobre como ter sucesso no mundo digital", "")
 
 corpo = corpo.replace("[&#8230;]", "...")
 
 text = '\n\n' + titulo + '\n\n' + corpo + '\n\nfonte: https://sucesso.hmr1973.com/ \n\nFollow me @hmr1973maia \n\n#bigdata #weworklabs #entrepreneur #innovation #cycling #datascience #innovation #technology #tech #design #business #engineering #startup #entrepreneur #science #entrepreneurship #future #marketing #creativity #architecture #sustainability #inspiration #ai #art #gadgets #digital #motivation #automation'
 
 font = ImageFont.truetype("Roboto-Medium.ttf", 60)
+font1 = ImageFont.truetype("Roboto-Medium.ttf", 40)
 
 textwrapped = textwrap.wrap(titulo, width=35)
 draw.text((0,0), '\n'.join(textwrapped), font=font, fill="#FFA500")
+
+textwrapped1 = textwrap.wrap(corpo1, width=50)
+draw.text((0,700), '\n'.join(textwrapped1), font=font1, fill="#FFA500")
 
 img.save('cur_time.jpg')
 
